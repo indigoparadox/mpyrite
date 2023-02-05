2e5711155a304db215c1b43e44490e80c4d06494
indigoparadox <indigo.repo@interfinitydynamics.info>
2023-02-05 15:11:40 -0500

    Fewer allocs in interpreter.

--- src/interp.c
+++ src/interp.c
@@ -10,129 +10,93 @@ int16_t interp_init( struct INTERP* interp, struct ASTREE* tree ) {
 
    memset( interp, '\0', sizeof( struct INTERP ) );
    interp->tree = tree;
-   interp->func_sz_max = 10;
-   interp->func_names = calloc( interp->func_sz_max, sizeof( char* ) );
-   if( NULL == interp->func_names ) {
+   interp->funcs_sz_max = 10;
+   interp->funcs =
+      calloc( interp->funcs_sz_max, sizeof( struct INTERP_FUNC ) );
+   if( NULL == interp->funcs ) {
       retval = -1;
       goto cleanup;
    }
-   interp->func_idxs = calloc( interp->func_sz_max, sizeof( int16_t ) );
-   if( NULL == interp->func_idxs ) {
-      retval = -1;
-      goto cleanup;
-   }
-   interp->func_cbs = calloc( interp->func_sz_max, sizeof( interp_func_cb ) );
-   if( NULL == interp->func_cbs ) {
+   interp->vars = calloc( interp->vars_sz_max, sizeof( struct INTERP_VAR ) );
+   if( NULL == interp->vars ) {
       retval = -1;
       goto cleanup;
    }
 
 cleanup:
 
-   if( NULL != interp->func_names && retval ) {
-      free( interp->func_names );
-      interp->func_names = NULL;
+   if( NULL != interp->funcs && retval ) {
+      free( interp->funcs );
+      interp->funcs = NULL;
    }
 
-   if( NULL != interp->func_idxs && retval ) {
-      free( interp->func_idxs );
-      interp->func_idxs = NULL;
-   }
-
-   if( NULL != interp->func_cbs && retval ) {
-      free( interp->func_cbs );
-      interp->func_cbs = NULL;
+   if( NULL != interp->vars && retval ) {
+      free( interp->vars );
+      interp->vars = NULL;
    }
 
    return 0;
 }
 
 void interp_free( struct INTERP* interp ) {
-   int16_t i = 0;
-
-   if( NULL != interp->func_names ) {
-      for( i = 0 ; i < interp->func_sz ; i++ ) {
-         free( interp->func_names[i] );
-      }
-      free( interp->func_names );
-      interp->func_names = NULL;
+   if( NULL != interp->funcs ) {
+      free( interp->funcs );
+      interp->funcs = NULL;
    }
 
-   if( NULL != interp->func_idxs ) {
-      free( interp->func_idxs );
-      interp->func_idxs = NULL;
-   }
-
-   if( NULL != interp->func_cbs ) {
-      free( interp->func_cbs );
-      interp->func_cbs = NULL;
+   if( NULL != interp->vars ) {
+      free( interp->vars );
+      interp->vars = NULL;
    }
 }
 
 int16_t interp_dbl_funcs( struct INTERP* interp ) {
-   int16_t* new_func_idxs = NULL;
-   char** new_func_names = NULL;
-   interp_func_cb* new_func_cbs = NULL;
+   struct INTERP_FUNC* new_funcs = NULL;
 
    debug_printf( 1, "function table limit exceeded; expanding to %d...",
-      interp->func_sz_max * 2 );
+      interp->funcs_sz_max * 2 );
    
    assert(
-      interp->func_sz_max * 2 * sizeof( char** ) >
-      interp->func_sz_max * sizeof( char** ) );
-   new_func_names = realloc(
-      interp->func_names, interp->func_sz_max * 2 * sizeof( char** ) );
-   assert( NULL != new_func_names );
-   interp->func_names = new_func_names;
+      interp->funcs_sz_max * 2 * sizeof( struct INTERP_FUNC ) >
+      interp->funcs_sz_max * sizeof( struct INTERP_FUNC ) );
+   new_funcs = realloc(
+      interp->funcs,
+      interp->funcs_sz_max * 2 * sizeof( struct INTERP_FUNC ) );
+   assert( NULL != new_funcs );
+   interp->funcs = new_funcs;
    
-   assert(
-      interp->func_sz_max * 2 * sizeof( int16_t ) >
-      interp->func_sz_max * sizeof( int16_t ) );
-   new_func_idxs = realloc(
-      interp->func_idxs, interp->func_sz_max * 2 * sizeof( int16_t ) );
-   assert( NULL != new_func_idxs );
-   interp->func_idxs = new_func_idxs;
-   
-   assert(
-      interp->func_sz_max * 2 * sizeof( interp_func_cb ) >
-      interp->func_sz_max * sizeof( interp_func_cb ) );
-   new_func_cbs = realloc(
-      interp->func_cbs, interp->func_sz_max * 2 * sizeof( interp_func_cb ) );
-   assert( NULL != new_func_cbs );
-   interp->func_cbs = new_func_cbs;
-
-   interp->func_sz_max *= 2;
+   interp->funcs_sz_max *= 2;
 
    /* TODO: Error handling beyond asserts() above. */
    return 0;
 }
 
 int16_t interp_set_func_pc(
-   struct INTERP* interp, const char* func_name, int16_t func_name_sz,
-   int16_t func_pc
+   struct INTERP* interp, const char* func_name, int16_t func_pc
 ) {
    int16_t i = 0;
 
-   for( i = 0 ; interp->func_sz > i ; i++ ) {
-      if( 0 == strcmp( func_name, interp->func_names[i] ) ) {
+   for( i = 0 ; interp->funcs_sz > i ; i++ ) {
+      if( 0 == strcmp( func_name, interp->funcs[i].name ) ) {
          debug_printf( 1, "function \"%s\" already exists, updating to %d...",
             func_name, func_pc );
-         interp->func_idxs[i] = func_pc;
+         interp->funcs[i].type = INTERP_FUNC_PC;
+         interp->funcs[i].exe.pc = func_pc;
          return 0;
       }
    }
 
-   if( interp->func_sz + 1 >= interp->func_sz_max ) {
+   if( interp->funcs_sz + 1 >= interp->funcs_sz_max ) {
       interp_dbl_funcs( interp );
    }
 
    /* Add the new function. */
    debug_printf( 1, "adding new function \"%s\", pc = %d", func_name, func_pc );
-   interp->func_names[interp->func_sz] = calloc( func_name_sz + 1, 1 );
-   assert( NULL != interp->func_names[interp->func_sz] );
-   strncpy( interp->func_names[interp->func_sz], func_name, func_name_sz );
-   interp->func_idxs[interp->func_sz] = func_pc;
-   interp->func_sz++;
+   strncpy( interp->funcs[interp->funcs_sz].name, func_name,
+      INTERP_FUNC_NAME_SZ_MAX );
+   interp->funcs[interp->funcs_sz].type = INTERP_FUNC_PC;
+   interp->funcs[interp->funcs_sz].exe.pc = func_pc;
+   interp->funcs_sz++;
 
    return 0;
 }
@@ -152,31 +116,34 @@ int16_t interp_set_func_def( struct INTERP* interp, struct ASTREE_NODE* def ) {
    /* TODO: Error handling. */
    assert( NULL != seq );
 
-   interp_set_func_pc( interp, def->value.s, strlen( def->value.s ), def_seq );
+   interp_set_func_pc( interp, def->value.s, def_seq );
 
    return 0;
 }
 
 int16_t interp_set_func_cb(
-   struct INTERP* interp, const char* func_name, int16_t func_name_sz,
-   interp_func_cb cb
+   struct INTERP* interp, const char* func_name, interp_func_cb cb
 ) {
    /* TODO: Some kind of wrapper to unstack vars. */
    return 0;
 }
 
 int16_t interp_set_var_int(
-   struct INTERP* interp, const char* var_name, int16_t var_name_sz,
-   int16_t value
+   struct INTERP* interp, const char* var_name, int16_t value
 ) {
 
    return 0;
 }
 
 int16_t interp_set_var_str(
-   struct INTERP* interp, const char* var_name, int16_t var_name_sz,
-   const char* value
+   struct INTERP* interp, const char* var_name, const char* value
 ) {
+   int16_t i = 0;
+
+   for( i = 0 ; interp->vars_sz ; i++ ) {
+
+   }
+
    return 0;
 }
 
@@ -216,11 +183,9 @@ int16_t interp_tick( struct INTERP* interp ) {
       right = astree_node( interp->tree, left->next_sibling );
       assert( NULL != right );
       if( ASTREE_VALUE_TYPE_INT == right->value_type ) {
-         interp_set_var_int( interp, left->value.s, strlen( left->value.s ),
-            atoi( right->value.s ) );
+         interp_set_var_int( interp, left->value.s, atoi( right->value.s ) );
       } else if( ASTREE_VALUE_TYPE_STRING == right->value_type ) {
-         interp_set_var_str( interp, left->value.s, strlen( left->value.s ),
-            right->value.s );
+         interp_set_var_str( interp, left->value.s, right->value.s );
       } else {
          retval = -1;
          goto cleanup;
--- src/interp.h
+++ src/interp.h
@@ -8,39 +8,50 @@ struct INTERP;
 
 typedef int16_t (*interp_func_cb)( struct INTERP* );
 
+#define INTERP_FUNC_NAME_SZ_MAX 255
 #define INTERP_VAR_NAME_SZ_MAX 255
+
+#define INTERP_FUNC_PC 0
+#define INTERP_FUNC_CB 1
 
 struct INTERP_VAR {
    char name[INTERP_VAR_NAME_SZ_MAX + 1];
    union ASTREE_NODE_VALUE value;  
 };
 
+union INTERP_FUNC_EXEC {
+   int16_t pc;
+   interp_func_cb cb;
+};
+
+struct INTERP_FUNC {
+   char name[INTERP_FUNC_NAME_SZ_MAX];
+   uint8_t type;
+   union INTERP_FUNC_EXEC exe;
+};
+
 struct INTERP {
-   char** func_names;
-   int16_t* func_idxs;
-   int16_t func_sz;
-   interp_func_cb* func_cbs;
-   int16_t func_sz_max;
+   struct INTERP_FUNC* funcs;
+   int16_t funcs_sz;
+   int16_t funcs_sz_max;
    int16_t pc;
    struct ASTREE* tree;
    /* TODO: Implement scope. */
    struct INTERP_VAR* vars;
    int16_t vars_sz;
+   int16_t vars_sz_max;
 };
 
 int16_t interp_init( struct INTERP* interp, struct ASTREE* tree );
 void interp_free( struct INTERP* interp );
 int16_t interp_dbl_funcs( struct INTERP* interp );
 int16_t interp_set_func_pc(
-   struct INTERP* interp, const char* func_name, int16_t func_name_sz,
-   int16_t func_pc );
+   struct INTERP* interp, const char* func_name, int16_t func_pc );
 int16_t interp_set_func_def( struct INTERP* interp, struct ASTREE_NODE* def );
 int16_t interp_set_var_int(
-   struct INTERP* interp, const char* var_name, int16_t var_name_sz,
-   int16_t value );
+   struct INTERP* interp, const char* var_name, int16_t value );
 int16_t interp_set_var_str(
-   struct INTERP* interp, const char* var_name, int16_t var_name_sz,
-   const char* value );
+   struct INTERP* interp, const char* var_name, const char* value );
 int16_t interp_tick( struct INTERP* interp );
 
 #endif /* !INTERP_H */
